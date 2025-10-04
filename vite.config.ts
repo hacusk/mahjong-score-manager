23b631683aa00761d33ebdd9f6c9d39631c9723a
hacusk <5301731+hacusk@users.noreply.github.com>
2025-10-04 18:31:38 +0900

    fix: base directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/mahjong-score-manager",
+  base: "/mahjong-score-manager/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
