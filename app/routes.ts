6b37e3dc4fe8463051b70e9c76a063c0d6aa70b1
hacusk <5301731+hacusk@users.noreply.github.com>
2025-10-04 23:11:27 +0900

    fix: route

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
   route(
-    "/mahjong-score-manager/",
+    "mahjong-score-manager",
     "routes/home.tsx",
   ),
 ] satisfies RouteConfig;
